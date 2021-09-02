57af2c891f681ca62818f3cc40e2afec4eade0d2
DevExpressExampleBot <fakeEmail>
2021-09-02 15:57:20 +0300

    Source auto update [skip ci]

--- VB/SplineChart/MainWindow.xaml.vb
+++ VB/SplineChart/MainWindow.xaml.vb
@@ -24,21 +24,21 @@ Namespace SplineChart
 			End Set
 		End Property
 		Public Sub New()
-			Dim lastYear As Integer = Date.Now.Year - 1
+			Dim lastYear As Integer = DateTime.Now.Year - 1
 			Data = New ObservableCollection(Of DataPoint) From {
-				New DataPoint(New Date(lastYear,1,1), 138.7),
-				New DataPoint(New Date(lastYear,2,1), 141.4),
-				New DataPoint(New Date(lastYear,3,1), 159.5),
-				New DataPoint(New Date(lastYear,4,1), 160.7),
-				New DataPoint(New Date(lastYear,5,1), 148.8),
-				New DataPoint(New Date(lastYear,6,1), 166.6)
+				New DataPoint(New DateTime(lastYear,1,1), 138.7),
+				New DataPoint(New DateTime(lastYear,2,1), 141.4),
+				New DataPoint(New DateTime(lastYear,3,1), 159.5),
+				New DataPoint(New DateTime(lastYear,4,1), 160.7),
+				New DataPoint(New DateTime(lastYear,5,1), 148.8),
+				New DataPoint(New DateTime(lastYear,6,1), 166.6)
 			}
 		End Sub
 	End Class
 	Public Class DataPoint
-		Public Property Argument() As Date
+		Public Property Argument() As DateTime
 		Public Property Value() As Double
-		Public Sub New(ByVal argument As Date, ByVal value As Double)
+		Public Sub New(ByVal argument As DateTime, ByVal value As Double)
 			Me.Argument = argument
 			Me.Value = value
 		End Sub
